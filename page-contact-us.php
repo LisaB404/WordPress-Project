0d5a7cac3f002172dbb95b809cae9301c9c69f2b
Lisa <lizardb96@gmail.com>
2025-03-23 15:12:12 +0100

    Form with plugin Contact Form 7

--- page-contact-us.php
+++ page-contact-us.php
@@ -2,26 +2,11 @@
 
 <div class="page-title"><?php the_title( '<h2>', '</h2>' ); ?></div>
 <div class="contact-container">
-<p>Fill out the form to get in touch with us.</p>
+    <p>Fill out the form to get in touch with us.</p>
 
-<div class="form-container">
-    <form action="submit.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
-
-        <label for="mail" class="form-label">E-mail</label>
-        <input type="mail" id="mail" name="email" placeholder="e.g. janedoe@gmail.com" required>
-
-        <label for="subject">Subject:</label>
-        <input type="text" id="subject" name="subject" placeholder="e.g. reservation request" required>
-
-        <label for="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-
-        <button type="reset">Clear</button>
-        <button type="submit">Send</button>
-    </form>
-</div>
+    <div class="form-container">
+        <?php echo do_shortcode('[contact-form-7 id="f8d312a" title="Pomelia Hotel Form"]'); ?>
+    </div>
 </div>
 
 <?php get_footer(); ?>
